702ba19ad7b8997122fd47bb6a64308d309facc9
Cristiano Franco <cristiano.f.escobar@gmail.com>
2023-01-08 11:29:38 -0400

    created src folder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../src/styles/globals.css";
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from "next/app";
 import theme from "../styles/theme";
