8d469e97faa192bcc8922de6d22e85b9bf4ec9ac
Cristiano Franco <cristiano.f.escobar@gmail.com>
2023-01-08 13:11:03 -0400

    changed songs object to array

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,29 +2,32 @@ import { Flex } from "@chakra-ui/react";
 import Player from "../components/player";
 
 export default function Home() {
-  const songs = {
-    first: {
+  const songs = [
+    {
+      id: 1,
       name: "????",
       artist: "EDEN",
       coverUrl: "/images/no-future.jpg",
       audio: "songs/question.mp3",
       duration: 179
     },
-    second: {
+    {
+      id: 2,
       name: "Modern Warfare",
       artist: "EDEN",
       coverUrl: "/images/icymi.jpg",
       audio: "songs/modern.mp3",
       duration: 202
     },
-    third: {
+    {
+      id: 3,
       name: "Untitled",
       artist: "EDEN",
       coverUrl: "/images/no-future.jpg",
       audio: "songs/untitled.mp3",
       duration: 216,
     },
-  };
+  ];
 
   return (
     <Flex
@@ -34,10 +37,10 @@ export default function Home() {
       justifyContent="center"
     >
       <Flex width="820px" justifyContent="space-between">
-        <Player size="lg" song={songs.first} />
+        <Player size="lg" song={songs[0]} />
         <Flex flexDirection="column" justifyContent="space-between">
-          <Player size="md" song={songs.second} />
-          <Player size="sm" song={songs.third} />
+          <Player size="md" song={songs[1]} />
+          <Player size="sm" song={songs[2]} />
         </Flex>
       </Flex>
     </Flex>
